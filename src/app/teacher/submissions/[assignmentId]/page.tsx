d071ce335c9ffa89b3e3ac825cde2baf90020ca2
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-08-24 21:55:28 -0300

    não sei se ta certo porque não ta nem conseguindo enviar as respostas de tarefa

--- src/app/teacher/submissions/[assignmentId]/page.tsx
+++ src/app/teacher/submissions/[assignmentId]/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import { useParams } from "next/navigation";
 import SubmissionReviewPage from "@/features/SubmissionReview/SubmissionReview";
 import TeacherAssignmentSubmissionsPage from "@/features/TeacherAssignment/TeacherAssignmentSubmissions";
 
-export default function Page({ params }: { params: { assignmentId: string } }) {
+export default function Page() {
+
+  const params = useParams()
+
+  const assignmentId = Number(params.assignmentId)
+  //loader para o usuário
+  if (isNaN(assignmentId)) {
+    return <div>Carregando...</div>
+  }
+  
   return (<>
-    <TeacherAssignmentSubmissionsPage assignmentId={Number(params.assignmentId)} />
+    <TeacherAssignmentSubmissionsPage assignmentId={assignmentId} />
     <SubmissionReviewPage />
   </>
   )
